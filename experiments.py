999caa47da55b4e250861dea8ca103c099e61bc5
idave2709 <idave@kellettschool.com>
2024-10-30 04:40:33 +0000

    save

---
+++ experiments.py
@@ -0,0 +1,7 @@
+sleepy = "😴"
+
+for i in range(3, 30, 3):
+    print(i, i * sleepy)
+    
+for i in range(30, 2, -3):
+    print(i, i * sleepy)
---
+++ main.py
@@ -0,0 +1,75 @@
+from os import system
+from time import sleep
+clear = lambda : system("cls")
+
+print("STARTING SPACESHIP TO THE ALIEN ZOO")
+sleep(1.00)
+print("3")
+sleep(1.00)
+print("2")
+sleep(1.00)
+print("1")
+sleep(1.00)
+
+blastoff = input("PRESS 'E' TO BLASTOFF!!")
+if blastoff == "e" or blastoff == "E":
+    print("BLASTOFF")
+    print("🚀     "*95)
+    sleep(1.00)
+    print("🔥     "*95) 
+    sleep(1.00)
+    print("🏙️     "*95)
+    sleep(1.00)
+    print("🌦️     "*95)
+    sleep(1.00)
+    print("⛅     "*95)
+    sleep(1.00)
+    print("🌠     "*95)
+    sleep(1.00)
+    print("⭐     "*95)
+    sleep(1.00)
+    print("Entering Mars...")
+    sleep(3.00)
+    print("👽" *95)
+    sleep(1.00)
+    print("👽" *95)
+    sleep(1.00)
+    print("👽" *95)
+    sleep(1.00)
+    print("👽👽👽 Welcome to Mars! Join me on a tour to the our Alien Zoo Zany. Make sure you to have your tickets!👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽👽")
+    print("👽" *95)
+    sleep(1.00)
+    print("👽" *95)
+    sleep(1.00)
+    print("👽" *95)
+    sleep(2.00)
+    print("""
+          NEW CHAPTER STARTING
+          """)
+    sleep(2.00)
+    print("Welcome to this tour. Our first animal is the Floatwhale!")
+    print("                                                                                                                                                                                                      ")
+    print("""    🌫️🌫️🌫️🌫️🌫️🌫️🌫️🌫️🌫️
+    🫧🫧💫💫💫💫💫💫💫💫🫧🫧
+    🌊🌊🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🌊🌊
+    🫧💫💫💫💫💫💫💫💫💫💫💫💫💫🫧
+    🌊🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🌊
+    🫧💫💫💫💫💫💫💫💫💫💫💫💫💫🫧
+    🌊🌊🐋🐋🐋🐋🐋🐋🐋🐋🐋🐋🌊🌊
+    🫧🫧💫💫💫💫💫💫💫💫🫧🫧
+    🌫️🌫️🌫️🌫️🌫️🌫️🌫️🌫️🌫️""""")
+    sleep(2.0)
+    print("The Floatwhale is a massive, translucent creature that drifts through gas giant atmospheres, filtering microorganisms with crystalline tendrils. Its body generates lifting gases to control altitude.")
+    sleep(1.0)
+    print("👽👽👽👽👽👽👽👽 Our next animal will be the Prismantis, lets go see it! 👽👽👽👽👽👽👽")
+    print("                                                                                                                                                                                                    ")
+    sleep(1.0)
+    print("""""" 🔮🔮🔮🔮🔮
+✨✨👁️💎💎💎👁️✨✨
+🦿🦿🦿🦿🦿🦿🦿
+🦿🦿🦿🦿🦿🦿🦿🦿🦿
+🦿🦿🦿🦿🦿🦿🦿🦿🦿🦿🦿
+⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️⚡️
+✨💫✨💫✨💫✨💫"""""")
+else:
+    print("Blastoff Failed, womp womp. (This is the end now. Enjoy your certified bozo certificate.)")
